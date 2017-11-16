21c272480088f7d72147b07f8f5f299dbb0640ad
skywindzz <skywindzz@yahoo.com>
2017-11-15 18:56:55 -0800

    11/15

--- client/components/comments.js
+++ client/components/comments.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 
+const renderComment = (comment, i) => {
+    return (
+        <div key={i} className="comment">
+            <p>
+                <strong>{comment.user}</strong>
+                {comment.text}
+                <button className="remove-comment">&times;</button>
+            </p>
+        </div>
+    )
+}
+
 class Comments extends Component {
-    render () {
+    render() {
         return (
-            <div className="comment">
-                this is comment sec
+            <div className="comments">
+                {this.props.postComments.map(renderComment)}
+                <form ref="commentForm" className="comment-form">
+                <input type="text" ref="author" placeholder="author" />
+                <input type="text" ref="comment" placeholder="comment" />
+                </form>
             </div>
+
         )    
     }
 }
-
 export default Comments;
--- client/components/single.js
+++ client/components/single.js
@@ -2,17 +2,22 @@ import React, { Component } from 'react';
 import Photo from './photo';
 import Comments from './comments';
 
-class Single extends Component { 
+class Single extends Component {
     render() {
         //need index of post
-        const i = this.props.posts.findIndex(post => post.code === this.props.params.postId)
+        const { postId } = this.props.params;
+        const i = this.props.posts.findIndex(post => post.code === postId)
         const post = this.props.posts[i];
 
+        //the || [] is for when a single photo page has no comments this way if you add one 
+        //you can add into that empty array also when need to map over it it won't show error if there is no comment
+        const postComments = this.props.comments[postId] || [];
+        console.log(postComments, "this is from single.js");
         //get the post  
         return (
             <div className="single-photo">
                 <Photo i={i} post={post} {...this.props} />        
-                <Comments />
+                <Comments postComments={postComments} />
             </div>
         )
     }
